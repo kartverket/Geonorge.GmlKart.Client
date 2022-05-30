142c83d2c0f82f499d05bcfbac2c6a8e50de5a5e
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2022-05-30 15:34:08 +0200

    Fjernet testkode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import axios from 'axios';
 import { Spinner } from 'components/custom-elements';
 import { MapView, SplashScreen, TopBar } from 'components/partials';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './App.scss';
 
 function App() {
    const [mapDocument, setMapDocument] = useState(null);
    const apiLoading = useSelector(state => state.api.loading);
-
-   useEffect(
-      () => {
-         axios.get('/data.json')
-            .then(response => {
-               setMapDocument(response.data);
-            });
-      },
-      []
-   );
 
    return (
       <div className={`app ${apiLoading ? 'api-loading' : ''}`}>
